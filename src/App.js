d9a0d253dbc741b5a06d50f6250c69d5375a3847
gelacio <gbsf.software.dev@gmail.com>
2020-10-27 12:21:28 -0600

    Added backimage code and implementing new styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import LoginContainer from './components/loginContainer'
+import LoginContainer from './components/loginContainer';
+import ImageContainer from './components/loginBackImage';
+import styled from 'styled-components';
+
+const Main = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`
 
 const App = () => {
     return (
-        <LoginContainer/>
+        <Main>
+            <ImageContainer Mimg={'/imgs/Captura.PNG'}></ImageContainer>
+            <LoginContainer/>
+        </Main>
     )
 }
 
--- src/components/loginContainer/loginContainer.js
+++ src/components/loginContainer/loginContainer.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import Input from '../input'
-import Button from '../button'
+import Input from '../input';
+import Button from '../button';
+import Label from '../label';
+import Title from '../title';
+import Message from '../message';
 import styled from 'styled-components';
 
 const Container = styled.div`
     margin: 0;
     padding:3rem 3rem;
+    border-radius: 0em 0.4em 0.4em 0em;
     width: 20%;
     height: 50%;
     display: flex;
@@ -15,36 +19,14 @@ const Container = styled.div`
     background-color:white;
 `;
 
-const Label = styled.label`
-    width:100%;
-    padding-bottom:0.5rem;
-    font-size:0.7rem;
-    font-weight:bold;
-    color:#4298f4;
-    text-align:left;
-`;
-
-const Title = styled.h2`
-    font-weight:bold;
-    color:#4298f4;
-`;
-
-const Message = styled.p`
-    /* font-weight:bold; */
-    width:100%;
-    text-align:left;
-    font-size:0.8em;
-    color:#4298f4;
-`;
-
-const LoginContainer = (elements) =>{
+const LoginContainer = () =>{
     return(
         <Container>
-            <Title>Log in</Title>
-            <Message>You can login with your actual account</Message>
-            <Label for={"userInput"}>{"User"}</Label>
+            <Title text={"Log in"}></Title>
+            <Message text={"You can login with your actual account"}></Message>
+            <Label lfor={"userInput"} text={"User"}></Label>
             <Input initialPH={""} id={"userInput"}  type={"text"}></Input>
-            <Label for={"passInput"}>{"Password"}</Label>
+            <Label lfor={"passInput"} text={"Password"}></Label>
             <Input initialPH={""} id={"passInput"} type={"password"}></Input>
             <Button text={"Log in"}></Button>
         </Container>
@@ -52,3 +34,4 @@ const LoginContainer = (elements) =>{
 }
 
 export default LoginContainer;
+
